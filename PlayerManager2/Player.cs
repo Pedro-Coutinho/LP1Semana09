2002f8634a8b7aa914e86938b9ad3c2218818c15
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-05-11 17:08:12 +0100

    Ex 4

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,15 @@
+namespace PlayerManager2
+{
+    public class Player
+    {
+        public string Name{get;}
+
+        public int Score{get;}
+        
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
