0391353ab415dfd02cf9964b7c4173e7e9f2f97e
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-05-11 17:02:40 +0100

    Ex3

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,15 @@
+namespace PlayerManager1
+{
+    public class Player
+    {
+        public string Name{get;}
+
+        public int Score{get;}
+        
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
